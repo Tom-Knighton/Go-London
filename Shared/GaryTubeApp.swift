aae2ff40992b06b47c5eaeb39338c727eb0d4274
Tom K <tomknighton@icloud.com>
2021-10-01 09:39:18 +0100

    Initial Commit

---
+++ Shared/GaryTubeApp.swift
@@ -0,0 +1,17 @@
+//
+//  GaryTubeApp.swift
+//  Shared
+//
+//  Created by Tom Knighton on 01/10/2021.
+//
+
+import SwiftUI
+
+@main
+struct GaryTubeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
