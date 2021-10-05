d237d8ce0bfcc465492c23530b73380be7ba6708
Tom K <tomknighton@icloud.com>
2021-10-05 22:18:35 +0100

    feat: StopPoint lineGroups and utilities

--- Shared/Models/StopPoint.swift
+++ Shared/Models/StopPoint.swift
@@ -6,6 +6,7 @@
 //
 
 import Foundation
+import SwiftUI
 
 struct StopPoint: Codable {
     let icsId: String?
@@ -13,8 +14,88 @@ struct StopPoint: Codable {
     let zone: String?
     let id: String?
     let name: String?
+    let commonName: String?
     let lat: Double?
     let lon: Double?
+    
+    let lineModeGroups: [LineModeGroup]?
+    
+    var lineIdentifiers: [LineIdentifier]? {
+        var ids: [LineIdentifier] = []
+        lineModeGroups?.forEach({ lineModeGroup in
+            if lineModeGroup.modeName == "tube" {
+                lineModeGroup.lineIdentifier?.forEach({ identifier in
+                    ids.append(LineIdentifier(lineId: identifier))
+                })
+            } else {
+                ids.append(LineIdentifier(lineId: lineModeGroup.modeName))
+            }
+        })
+        return ids
+    }
+}
+
+struct LineModeGroup: Codable {
+    let modeName: String?
+    let lineIdentifier: [String]?
+}
+
+struct LineIdentifier: Codable {
+    let lineId: String?
+    
+    @ViewBuilder
+    var lineIndicator: some View {
+        switch lineId {
+        case "national-rail":
+            Image(systemName: "tram")
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+                .frame(width: 25, height: 25)
+        case "bus":
+            Image(systemName: "bus")
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+                .frame(width: 25, height: 25)
+        default:
+            RoundedRectangle(cornerRadius: 10)
+                .frame(width: 25, height: 25)
+                .foregroundColor(colour)
+        }
+        
+    }
+    
+    private var colour: Color {
+        switch lineId {
+        case "overground":
+            return Color(hex: 0xEF7C09)
+        case "tflrail":
+            return Color(hex: 0x604099)
+        case "bakerloo":
+            return Color(hex: 0xB05F0F)
+        case "central":
+            return Color(hex: 0xEE2E21)
+        case "circle":
+            return Color(hex: 0xFED203)
+        case "district":
+            return Color(hex: 0x00853D)
+        case "hammersmith-city":
+            return Color(hex: 0xF4879F)
+        case "jubilee":
+            return Color(hex: 0x949CA0)
+        case "metropolitan":
+            return Color(hex: 0x96005E)
+        case "northern":
+            return Color(hex: 0x231F20)
+        case "piccadilly":
+            return Color(hex: 0x1B3F94)
+        case "victoria":
+            return Color(hex: 0x049EDC)
+        case "waterloo-city":
+            return Color(hex: 0x84CDBC)
+        default:
+            return Color.clear
+        }
+    }
 }
 
 struct StopPointQueryResult: Codable {
