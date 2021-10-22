009ed86cc9f1abe936e9a48bd6fced971386c08c
Tom K <tomknighton@icloud.com>
2021-10-22 14:04:37 +0100

    feat(StopPoint): Arrivals overview on StopPointOverview

--- Shared/Views/StopPointOverview.swift
+++ Shared/Views/StopPointOverview.swift
@@ -10,6 +10,19 @@ import MapKit
 import CoreLocation
 import CoreLocationUI
 
+class ArrivalGroupObserver: ObservableObject {
+    
+    @Published var arrivalGroups: [ArrivalGroup] = []
+    
+    func updateArrivalGroups(for stopPoint: StopPoint) async {
+        DispatchQueue.main.async {
+            Task {
+                self.arrivalGroups = await StopPointService.GetEstimatedArrivals(for: stopPoint)
+            }
+        }
+    }
+}
+
 
 struct StopPointOverview: View {
     
@@ -19,32 +32,42 @@ struct StopPointOverview: View {
     @Environment(\.safeAreaInsets) var edges
     @Namespace var animation
     
+    @ObservedObject var arrivalGroupObserver = ArrivalGroupObserver()
+    
+    let arrivalsTimer = Timer.publish(every: 30.0, on: .main, in: .common).autoconnect()
+    
     var body: some View {
         ZStack {
             VStack(spacing: 0) {
                 GaryGoHeaderView(headerTitle: stopPoint.commonName ?? "Station", colour: Color.blue)
                 
                 ScrollView {
-                    if let lat = stopPoint.lat,
-                       let long = stopPoint.lon {
-                        let region = MKCoordinateRegion(center: CLLocationCoordinate2D(latitude: lat, longitude: long), span: MKCoordinateSpan(latitudeDelta: 0.015, longitudeDelta: 0.015))
-                        
-                        Button(action: {
-                            withAnimation(Animation.interactiveSpring()) {
-                                self.showFullMapView.toggle()
+                    VStack {
+                        if let lat = stopPoint.lat,
+                           let long = stopPoint.lon {
+                            let region = MKCoordinateRegion(center: CLLocationCoordinate2D(latitude: lat, longitude: long), span: MKCoordinateSpan(latitudeDelta: 0.015, longitudeDelta: 0.015))
+                            
+                            Button(action: {
+                                withAnimation(Animation.interactiveSpring()) {
+                                    self.showFullMapView.toggle()
+                                }
+                            }) {
+                                GGMapView(region: .constant(region), annotations: [GGMapAnnotation(title: stopPoint.commonName ?? "", subtitle: nil, coordinate: region.center, iconName: "location.circle")])
+                                    .cornerRadius(15)
+                                    .padding(.top, 16)
+                                    .padding(.horizontal, 16)
+                                    .frame(maxWidth: .infinity, minHeight: 250, alignment: .center)
+                                    .shadow(radius: 5)
+                                    .allowsHitTesting(false)
                             }
-                        }) {
-                            GGMapView(region: .constant(region), annotations: [GGMapAnnotation(title: stopPoint.commonName ?? "", subtitle: nil, coordinate: region.center, iconName: "location.circle")])
-                                .cornerRadius(15)
-                                .padding(.top, 16)
-                                .padding(.horizontal, 16)
-                                .frame(maxWidth: .infinity, minHeight: 250, alignment: .center)
-                                .shadow(radius: 5)
-                                .allowsHitTesting(false)
+                        }
+                        
+                        StopPointInfoCard(stopPointInfo: stopPoint.getStopPointInfo())
+                        
+                        ForEach(self.arrivalGroupObserver.arrivalGroups, id: \.lineName) { arrivalGroup in
+                            StopPointArrivalsard(lineName: arrivalGroup.lineName, arrivals: arrivalGroup.getPlatformArrivalGroups())
                         }
                     }
-                    
-                    StopPointInfoCard(stopPointInfo: stopPoint.getStopPointInfo())
                 }
                 Spacer()
             }
@@ -56,7 +79,23 @@ struct StopPointOverview: View {
         }
         .navigationBarHidden(true)
         .background(Color("Section"))
-        
+        .onReceive(arrivalsTimer, perform: { _ in
+            Task {
+                await loadArrivalsHere()
+            }
+        })
+        .onReceive(NotificationCenter.default.publisher(for: UIApplication.willEnterForegroundNotification), perform: { _ in
+            Task {
+                await loadArrivalsHere()
+            }
+        })
+        .task {
+            await loadArrivalsHere()
+        }
+    }
+    
+    func loadArrivalsHere() async {
+        await self.arrivalGroupObserver.updateArrivalGroups(for: self.stopPoint)
     }
     
     @ViewBuilder
@@ -129,3 +168,82 @@ fileprivate struct StopPointInfoCard: View {
         }
     }
 }
+
+fileprivate struct StopPointArrivalsard: View {
+    
+    var lineName: String
+    var arrivals: [ArrivalGroup.PlatformArrivalGroup]
+    
+    var body: some View {
+        VStack(spacing: 16) {
+            HStack {
+                Text(lineName)
+                    .font(.title2)
+                    .bold()
+                    .frame(maxWidth: .infinity, alignment: .leading)
+                    .padding()
+            }
+            .frame(maxWidth: .infinity, alignment: .leading)
+            .background(GaryTubeConstants.getLineColour(from: lineName).overlay(Material.thin).shadow(radius: 5))
+            
+            ForEach(arrivals, id: \.platformName) { arrivalGroup in
+                HStack {
+                    VStack {
+                        Text(arrivalGroup.direction)
+                            .bold()
+                            .frame(maxWidth: .infinity, alignment: .leading)
+                        Text(arrivalGroup.platformName)
+                            .frame(maxWidth: .infinity, alignment: .leading)
+                    }
+                    
+                    arrivalTimes(arrivals: arrivalGroup.arrivals)
+                    Spacer()
+                }
+                .font(.title3)
+                .padding(.horizontal, 8)
+            }
+            Spacer().frame(height: 8)
+        }
+        .frame(maxWidth: .infinity, alignment: .center)
+        .background(Color("Section2"))
+        .cornerRadius(15)
+        .shadow(radius: 5)
+        .padding(.horizontal, 16)
+        .padding(.vertical, 8)
+    }
+    
+    @ViewBuilder
+    func arrivalTimes(arrivals: [StopPointArrival]) -> some View {
+        VStack {
+            if arrivals.count >= 1, let firstArrival = arrivals.first {
+                Text(getNextArrivalText(for: firstArrival))
+                    .bold()
+                    .font(.system(size: 19, design: .rounded))
+                    .frame(maxWidth: .infinity, alignment: .trailing)
+                Text(getNextThreeArrivalsText(for:arrivals))
+                    .frame(maxWidth: .infinity, alignment: .trailing)
+            } else {
+                Text("Check Station Boards")
+            }
+        }
+        .font(.system(size: 14, weight: .light, design: .rounded))
+        .multilineTextAlignment(.trailing)
+    }
+    
+    func getNextArrivalText(for arrival: StopPointArrival) -> String {
+        return arrival.friendlyDueTime == "Due" ? "Due" : arrival.friendlyDueTime + " min"
+    }
+    
+    func getNextThreeArrivalsText(for arrivals: [StopPointArrival]) -> String {
+        
+        guard arrivals.count > 1 else { return "" }
+        let maxIndex = 3 >= arrivals.count ? arrivals.count - 1 : 3
+        var friendlyText = "Then "
+        
+        for i in 1...maxIndex {
+            friendlyText += arrivals[i].friendlyDueTime + (i == maxIndex ? " mins" : ", ")
+        }
+        return friendlyText
+    }
+}
+
