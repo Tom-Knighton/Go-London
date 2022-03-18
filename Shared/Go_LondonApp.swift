216e9613acaa8987b073564b7c4287636a74c251
Tom K <tomknighton@icloud.com>
2022-03-18 11:55:45 +0000

    Blank Project

---
+++ Shared/Go_LondonApp.swift
@@ -0,0 +1,17 @@
+//
+//  Go_LondonApp.swift
+//  Shared
+//
+//  Created by Tom Knighton on 18/03/2022.
+//
+
+import SwiftUI
+
+@main
+struct Go_LondonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
