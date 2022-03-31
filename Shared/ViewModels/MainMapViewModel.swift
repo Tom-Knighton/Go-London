1481e43a410506addbd88e85795c5a339bc5bf63
Tom K <tomknighton@icloud.com>
2022-04-01 00:11:42 +0100

    feat(map): Added viewmodel for main map + Added buttons on top of map for filters + filter logic

---
+++ Shared/ViewModels/MainMapViewModel.swift
@@ -0,0 +1,80 @@
+//
+//  MainMapViewModel.swift
+//  Go London (iOS)
+//
+//  Created by Tom Knighton on 31/03/2022.
+//
+
+import Foundation
+import CoreLocation
+import GoLondonSDK
+import Combine
+
+class MainMapViewModel: ObservableObject {
+    
+    @Published var centerLocation: CLLocationCoordinate2D
+    @Published var radius: Float
+    @Published var filters: LineModeFilters
+    @Published var nearbyMarkers: [StopPointAnnotation]
+    
+    class LineModeFilters: ObservableObject {
+        struct Filter: Identifiable {
+            var lineMode: LineMode
+            var toggled: Bool = true
+            
+            var id: String { lineMode.rawValue }
+        }
+        
+        @Published var filters: [Filter] = []
+        
+        init(_ lineModes: [LineMode]) {
+            self.filters = lineModes.map { Filter(lineMode: $0) }
+        }
+        
+        func toggleFilter(_ lineMode: LineMode) {
+            if let index = self.filters.firstIndex(where: { $0.lineMode == lineMode }) {
+                self.filters[index].toggled.toggle()
+            }
+        }
+        
+        func isToggled(_ lineMode: LineMode) -> Bool {
+            return self.filters.first(where: { $0.lineMode == lineMode})?.toggled ?? false
+        }
+    }
+    
+    private static var defaultFilters: LineModeFilters =  LineModeFilters([LineMode.bus, LineMode.tflrail, LineMode.tube, LineMode.overground, LineMode.nationalRail, LineMode.dlr])
+    
+    var anyCancellable: AnyCancellable? = nil
+    
+    init(centerLocation: CLLocationCoordinate2D, radius: Float = 1000, filters: LineModeFilters = defaultFilters) {
+        self.centerLocation = centerLocation
+        self.radius = radius
+        self.filters = filters
+        self.nearbyMarkers = []
+        
+        anyCancellable = self.filters.objectWillChange.sink { [weak self] (_) in
+            self?.objectWillChange.send()
+        }
+        
+    }
+    
+    func toggleLineModeFilter(_ lineMode: LineMode) {
+        self.filters.toggleFilter(lineMode)
+    }
+    
+    func isFilterToggled(_ lineMode: LineMode) -> Bool {
+        return self.filters.isToggled(lineMode)
+    }
+    
+    func searchForMarkers() async {
+        let nearbyPoints = await GLSDK.Search.SearchAround(latitude: self.centerLocation.latitude, longitude: self.centerLocation.longitude)
+        DispatchQueue.main.async {
+            self.nearbyMarkers.removeAll()
+            for point in nearbyPoints {
+                if let point = point as? StopPoint {
+                    self.nearbyMarkers.append(StopPointAnnotation(stopPoint: point))
+                }
+            }
+        }
+    }
+}
--- Shared/Views/Map Page/HomeView.swift
+++ Shared/Views/Map Page/HomeView.swift
@@ -14,38 +14,42 @@ public struct HomeView : View {
     
     @Environment(\.colorScheme) var colourScheme
     
-    @State var nearStopPointMarkers: [StopPointAnnotation] = []
-    @State var mapCenter: CLLocationCoordinate2D = LocationManager.shared.lastLocation?.coordinate ?? GoLondon.LiverpoolStreetCoord
-    @StateObject var coord = TestModel()
+    @ObservedObject var model: MainMapViewModel = MainMapViewModel(centerLocation: LocationManager.shared.lastLocation?.coordinate ?? GoLondon.LiverpoolStreetCoord)
     
     var mapboxStyleURI: Binding<StyleURI> { Binding(get: { colourScheme == .dark ? GoLondon.GetDarkStyleURL() : GoLondon.GetLightStyleURL()}, set: { _ in })}
     
     public var body: some View {
         ZStack {
-            MapViewRepresentable(mapStyleURI: mapboxStyleURI, mapCenter: $mapCenter, markers: $nearStopPointMarkers, enableCurrentLocation: true)
+            MapViewRepresentable(mapStyleURI: mapboxStyleURI, mapCenter: $model.centerLocation, markers: $model.nearbyMarkers, enableCurrentLocation: true)
                 .onAppear {
                     search()
                 }
                 .edgesIgnoringSafeArea(.all)
-                .onChange(of: self.nearStopPointMarkers) { newValue in
-                    print("NEW MARKERS: \(newValue)")
-                }
             
-            Button(action: { search() }) {
-                Text("Search")
-            }.buttonStyle(BorderedProminentButtonStyle())
+            VStack {
+                Spacer().frame(height: 16)
+                ScrollView(.horizontal, showsIndicators: false) {
+                    LazyHStack {
+                        Button(action: {}) { Text("Test") }
+                        .buttonStyle(MapButtonStyle())
+                        ForEach(self.model.filters.filters, id: \.lineMode) { modeFilter in
+                            Button(action: { self.model.filters.toggleFilter(modeFilter.lineMode) }) {
+                                Text("\(modeFilter.lineMode.friendlyName): \(String(modeFilter.toggled))")
+                            }
+                            .buttonStyle(MapButtonStyle())
+                        }
+                    }
+                    .padding(.horizontal, 16)
+                }
+                .frame(maxWidth: .infinity, maxHeight: 60)
+                Spacer()
+            }
         }
     }
     
     func search() {
         Task {
-            let stopPoints = await GLSDK.Search.SearchAround(latitude: mapCenter.latitude, longitude: mapCenter.longitude, radius: 1000)
-            self.nearStopPointMarkers.removeAll()
-            for point in stopPoints {
-                if let point = point as? StopPoint {
-                    self.nearStopPointMarkers.append(StopPointAnnotation(stopPoint: point))
-                }
-            }
+            await self.model.searchForMarkers()
         }
     }
 }
