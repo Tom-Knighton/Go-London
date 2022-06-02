775ceb387210ad16838df4ddbb5474ab9a36c0b1
Tom K <tomknighton@icloud.com>
2022-06-02 16:07:06 +0100

    tweak(lines): Embed AllLines page in VStack to fix navigation title

--- Shared/Pages/Status Page/AllLinesStatusPage.swift
+++ Shared/Pages/Status Page/AllLinesStatusPage.swift
@@ -15,34 +15,37 @@ struct AllLinesStatusPage: View {
     @State var hasInit: Bool = false
     
     var body: some View {
-        ScrollView {
-            VStack {
-                Spacer()
-                if viewModel.isLoading {
-                    VStack {
-                        Spacer()
-                        LottieView(name: "Dog_PurpleWalking", loopMode: .loop)
-                            .frame(width: 200, height: 200, alignment: .center)
-                        Text("Loading...")
-                            .bold()
-                            .frame(maxWidth: .infinity, alignment: .center)
-                        Spacer()
-                    }
-                } else {
-                    self.overviewTooltip()
-                    LazyVStack {
-                        ForEach(viewModel.lines, id: \.id) { line in
-                            NavigationLink(destination: NavigationLazyView(LinePage(viewModel: LineStatusViewModel(for: line)))) {
-                                LineOverviewRow(line: line)
+        VStack {
+            ScrollView {
+                VStack {
+                    Spacer()
+                    if viewModel.isLoading {
+                        VStack {
+                            Spacer()
+                            LottieView(name: "Dog_PurpleWalking", loopMode: .loop)
+                                .frame(width: 200, height: 200, alignment: .center)
+                            Text("Loading...")
+                                .bold()
+                                .frame(maxWidth: .infinity, alignment: .center)
+                            Spacer()
+                        }
+                    } else {
+                        self.overviewTooltip()
+                        LazyVStack {
+                            ForEach(viewModel.lines, id: \.id) { line in
+                                NavigationLink(destination: NavigationLazyView(LinePage(viewModel: LineStatusViewModel(for: line)))) {
+                                    LineOverviewRow(line: line)
+                                }
                             }
                         }
                     }
+                    Spacer()
+                    Spacer().frame(height: 16)
                 }
-                Spacer()
-                Spacer().frame(height: 16)
+                .padding(.horizontal, 16)
             }
-            .padding(.horizontal, 16)
         }
+        .navigationTitle("TfL Status:")
         .background(Color.layer1.edgesIgnoringSafeArea(.all))
         .onAppear {
             Task {
